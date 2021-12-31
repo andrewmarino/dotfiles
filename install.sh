ddcca94d0dd0a70c174016adf88fbd1eda12aaac
Andrew Marino <andrew@andrewmarino.net>
2021-12-31 18:44:21 -0500

    update install.sh

--- install.sh
+++ install.sh
@@ -2,10 +2,19 @@
 
 echo "Starting install script..."
 
+# Check for Oh My Zsh and install if we don't have it
+if test ! $(which omz); then
+/bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
+fi
+
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
+
+# Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from our dotfiles
+rm -rf $HOME/.zshrc
+ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 
 # Update the formulae and Homebrew itself
 brew update
@@ -17,12 +26,11 @@ brew bundle
 # Install global Composer packages
 /usr/local/bin/composer global require laravel/valet
 
+# Install Laravel Valet
+$HOME/.composer/vendor/bin/valet install
+
 # Create directories for projects
 mkdir $HOME/Local
 mkdir $HOME/Valet
-
-# Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from our dotfiles
-rm -rf $HOME/.zshrc
-ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 
 echo "Install script complete."
