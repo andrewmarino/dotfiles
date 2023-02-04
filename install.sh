35c462bebedbc9d6dd31a14de538c27362fd831a
Andrew Marino <andrew@andrewmarino.net>
2023-02-03 21:54:08 -0500

    update install.sh for homebrew and zsh

--- install.sh
+++ install.sh
@@ -2,12 +2,6 @@
 
 echo "Starting install script..."
 
-# Ask for the administrator password upfront
-sudo -v
-
-# Keep-alive: update existing `sudo` time stamp until the script has finished
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-
 # Check for Oh My Zsh and install if we don't have it
 if test ! $(which omz); then
     /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
@@ -16,7 +10,14 @@ fi
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
+
+# Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
+rm -rf $HOME/.zshrc
+ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 
 # Check for NVM and install if we don't have it
 if test ! $(which nvm); then
